1a327929cf97fa021705361cc9d2f76e4860801f
osorosiku <sagasyunta@gmail.com>
2024-09-04 16:18:56 +0900

    歩行+障害物
    自動で前に移動するプログラムを追加
    障害物にあたった場合の処理を追加

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -15,17 +15,18 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+
+
+
     void FixedUpdate()
     {
         // 入力値を取得
         float hori = Input.GetAxis("Horizontal"); // A, Dキーの入力を取得
-        float vert = Input.GetAxis("Vertical");   // W, Sキーの入力を取得
 
-        // 入力に基づいて移動ベクトルを計算
-        Vector3 movement = (transform.right * hori) + (transform.forward * vert);
+        // 常に前に進むベクトルを追加
+        Vector3 movement = transform.forward + (transform.right * hori);
 
         // Rigidbodyの速度を設定
         rb.velocity = movement * forceAmount;
     }
 }
-//gitbranch-test
